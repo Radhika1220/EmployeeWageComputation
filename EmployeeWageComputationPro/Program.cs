0454134ddd22fc625d149903b168ce9f7eb9f88b
Radhika <radhika.shankar1220@gmail.com>
2021-06-28 22:37:27 +0530

    Calculating wages based on condition

--- EmployeeWageComputationPro/Program.cs
+++ EmployeeWageComputationPro/Program.cs
@@ -9,18 +9,23 @@ namespace EmployeeWageComputationPro
         public const int PART_TIME = 2;
         public const int WAGE_PER_HOUR = 20;
         public const int WORKING_PER_MONTH = 20;
+        public const int MAX_WORKING_DAYS = 20;
+        public const int MAX_WORKING_HRS = 100;
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to employee wage computation");
             //Creating a Random Function
             int empHours = 0;
             int empDailyWage = 0;
+            int workingDays = 1;
+            int workingHrs = 0;
             int totalWage = 0;
 
             Random r = new Random();
            
-            for(int day = 1; day <= WORKING_PER_MONTH; day++) 
+            while(workingDays<=MAX_WORKING_DAYS && workingHrs<=MAX_WORKING_HRS)
             {
+                //Calling the next method in Random Class
                 int empAttendance = r.Next(0, 3);
                 switch (empAttendance)
               {
@@ -39,9 +44,16 @@ namespace EmployeeWageComputationPro
              }
             empDailyWage = empHours * WAGE_PER_HOUR;
             totalWage += empDailyWage;
+            workingHrs += empHours;
+                if (empAttendance != 0)
+                {
+                    workingDays++;
+                }
         }
-            Console.WriteLine("Employee Wage Per day :" + empDailyWage);
-            Console.WriteLine("Employee Wage for 20 working days :" + totalWage);
+            Console.WriteLine("Working days in a month :" + workingDays);
+            Console.WriteLine("Working Hours :" + workingHrs);
+            //Console.WriteLine("Employee Wage Per day :" + empDailyWage);
+           // Console.WriteLine("Employee Wage for 20 working days :" + totalWage);
         }
     }
 }
