4c8a5ff9c9f8aaa92ab1a1bf5545d58524297248
Radhika <radhika.shankar1220@gmail.com>
2021-07-01 12:39:33 +0530

    [Radhika] Adding a method in class

--- EmployeeWageComputationPro/Program.cs
+++ EmployeeWageComputationPro/Program.cs
@@ -11,7 +11,7 @@ namespace EmployeeWageComputationPro
         public const int WORKING_PER_MONTH = 20;
         public const int MAX_WORKING_DAYS = 20;
         public const int MAX_WORKING_HRS = 100;
-        static void Main(string[] args)
+        public static void ComputeWage()
         {
             Console.WriteLine("Welcome to employee wage computation");
             //Creating a Random Function
@@ -22,39 +22,44 @@ namespace EmployeeWageComputationPro
             int totalWage = 0;
 
             Random r = new Random();
-           
-            while(workingDays<=MAX_WORKING_DAYS && workingHrs<=MAX_WORKING_HRS)
+
+            while (workingDays <= MAX_WORKING_DAYS && workingHrs <= MAX_WORKING_HRS)
             {
                 //Calling the next method in Random Class
                 int empAttendance = r.Next(0, 3);
                 switch (empAttendance)
-              {
-                case FULL_TIME:
-                   // Console.WriteLine("Employee is present for full-time");
-                    empHours = 8;
-                    break;
-                case PART_TIME:
-                    //Console.WriteLine("Employee is present for part-time");
-                    empHours = 4;
-                    break;
-                default:
-                    //Console.WriteLine("Employee is absent");
-                    break;
+                {
+                    case FULL_TIME:
+                        // Console.WriteLine("Employee is present for full-time");
+                        empHours = 8;
+                        break;
+                    case PART_TIME:
+                        //Console.WriteLine("Employee is present for part-time");
+                        empHours = 4;
+                        break;
+                    default:
+                        //Console.WriteLine("Employee is absent");
+                        break;
 
-             }
-            empDailyWage = empHours * WAGE_PER_HOUR;
-            totalWage += empDailyWage;
-            workingHrs += empHours;
+                }
+                empDailyWage = empHours * WAGE_PER_HOUR;
+                totalWage += empDailyWage;
+                workingHrs += empHours;
                 if (empAttendance != 0)
                 {
                     workingDays++;
                 }
-        }
+            }
             Console.WriteLine("Working days in a month :" + workingDays);
             Console.WriteLine("Working Hours :" + workingHrs);
-            //Console.WriteLine("Employee Wage Per day :" + empDailyWage);
-           // Console.WriteLine("Employee Wage for 20 working days :" + totalWage);
+            Console.WriteLine("Employee Wage Per day :" + empDailyWage);
+            Console.WriteLine("Employee Wage for 20 working days :" + totalWage);
+
         }
-    }
+         static  void Main(string[] args)
+        {
+            ComputeWage();
+        }
+    }  
 }
 
